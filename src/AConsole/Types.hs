1001515712132271737cd36bb881af7478af2108
Fernando Freire <dogonthehorizon@gmail.com>
2015-11-07 10:41:34 -0800

    Implement predicate for validating aws role names

--- src/AConsole/Types.hs
+++ src/AConsole/Types.hs
@@ -12,7 +12,7 @@ module AConsole.Types
       ARN
     ) where
 
-import Data.Char (isDigit, isAlphaNum)
+import Data.Char
 
 -- | Describes a valid account id according to AWS.
 data AccountId = AccountId String
@@ -36,10 +36,24 @@ data AWSRole = AWSRole String
 instance Show AWSRole where
     show (AWSRole a) = a
 
+-- | Predicate returning True if the given character is allowed in AWS
+-- role names.
+--
+-- TODO: Its possible that some unwanted chars can be validated against
+--       these categories. Write some tests to ensure this doesn't happen.
+isRoleChar c = case generalCategory c of
+    UppercaseLetter         -> True  -- A-Z
+    LowercaseLetter         -> True  -- a-z
+    DashPunctuation         -> True  -- -
+    ConnectorPunctuation    -> True  -- _
+    OtherPunctuation        -> True  -- @, ., ,
+    MathSymbol              -> True  -- +, =
+    _                       -> False -- Reject everything else
+
 -- | Constructor for a valid AWS role given the following constraints.
 -- http://docs.aws.amazon.com/IAM/latest/UserGuide/reference_iam-limits.html
 awsRole :: String -> AWSRole
-awsRole a | all isAlphaNum a = AWSRole a
+awsRole a | all isRoleChar a = AWSRole a
           | otherwise = error "Invalid AWS role name."
 
 type ARN = String
